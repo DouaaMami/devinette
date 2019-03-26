3fc7026c7b29605f841ac8a3ab7ad4305cb8e1c4
Douaa <Douaamami@gmail.com>
2019-03-26 19:05:02 +0100

    faire les condition et fermer la boucle

--- dev.js
+++ dev.js
@@ -1,6 +1,14 @@
 console.log('Bienvenue au jeu');
 var sol=Math.floor(Math.random()*100);
+console.log(sol);
 var tent=0;
-do{
-    var nb=prompt('donner un numero');
-}
+var nb=Number(prompt('donner un numero')); 
+while ((nb!== sol) && (tent<8))
+   { 
+    if (nb<sol) 
+    {console.log('faux! vous avez donné un nombre plus petit que la solution ');}
+    else 
+    {console.log('faux! vous avez donné un nombre plus grand que la solution ');}
+    tent++;
+    var nb=Number(prompt('donner un numero'))
+} 
